1e8402e69591bbcd50da0bf67a183e177a2f444b
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-06-17 18:18:25 +0530

    complete files

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const taskInput = document.getElementById("item-input");
+const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
 taskInput.addEventListener("keypress", e => {
   if (e.key === "Enter") addTask();
 });
+
+function addTask() {
+  const text = taskInput.value.trim();
+  if (!text) return;
+
+  taskList.innerHTML += `
+    <li>
+      <div class="task">
+        <input type="checkbox" onchange="this.parentElement.classList.toggle('done')">
+        <span>${text}</span>
+      </div>
+      <button class="delete" onclick="this.parentElement.remove()">🗑️</button>
+    </li>
+  `;
+  taskInput.value = "";
+}
+
+
 
